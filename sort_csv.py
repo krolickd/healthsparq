b5411b9d9baed8485ba6c8e25e01b0dac901f5c7
David Krolick <davidkrolick@gmail.com>
2021-03-29 14:29:04 -0400

    added comments and data validation

--- sort_csv.py
+++ sort_csv.py
@@ -1,30 +1,48 @@
 import pandas as pd
 from sys import argv
+from os import path
 
 
 def read_csv(file):
+    #convert csv file to pandas dataset
     data = pd.read_csv(file)
 
     return data
 
 
 def sort_data(data, column, sort_ascend):
-    data.sort_values(by=[column], inplace=sort_ascend)
+    #sort data set by provided column header
+    #Sorting specified as descending
+    data.sort_values(by=[column], inplace=True, ascending=sort_ascend)
 
     return data
 
 
 def write_csv(data):
-    data.to_csv("output.csv", index=False)
+    #write dataset to output.csv file
+    return data.to_csv("output.csv", index=False)
 
 
 if __name__ == '__main__':
+    #check for correct number of args
     num_args = len(argv)
-    in_file = argv[1]
-    sort_column = argv[2]
+    if num_args < 2:
+        print("Please include column heading to sort by as first argument")
+    else:
+        in_file = 'input.csv'
+        sort_column = argv[1]
 
-    data = read_csv(in_file)
 
-    sorted_data = sort_data(data, sort_column, True)
+        if not path.isfile('input.csv'):
+            print('File input.csv is required for input')
+        else:
+            data = read_csv(in_file)
 
-    write_csv(sorted_data)
+            if sort_column not in data.head():
+                print("Please enter a column heading in the dataset")
+            else:
+
+                sorted_data = sort_data(data, sort_column, False)
+
+                if write_csv(sorted_data):
+                    print('Error wrtiting file')
